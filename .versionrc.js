836b15bd029a4e5b638aa15f58a7d7325abdc19c
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-08-25 17:39:25 +0700

    chore: cleanup and updates

--- .versionrc.js
+++ .versionrc.js
@@ -1,10 +1,13 @@
-const defaultStandardVersion = require("@davidsneighbour/standard-version-config");
+const defaultStandardVersion = require("@davidsneighbour/config/standard-version");
 
 const localStandardVersion = {
   bumpFiles: [
     ...defaultStandardVersion.bumpFiles,
     { filename: "data/dnb/asylum/build.json", type: "json" },
   ],
+  skip: [
+    "changelog"
+  ]
 };
 
 module.exports = {
